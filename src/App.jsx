73c7544d322e138a6effba7f415efa6881e9bbe6
Adham <zubairadham.ar@gmail.com>
2023-07-18 10:16:43 +0800

    added custom styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-4xl text-cyan-300 font-bold underline">
+    <h1 className="text-4xl text-secondary-500 font-bold underline">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        200: "#95e7e7",
+        500: "#2acfcf",
+      },
+      secondary: {
+        500: '#3b3054',
+      },
+      neutral: {
+        200: "hsl(0, 0%, 75%)",
+        300: "hsl(257, 7%, 63%)",
+        700: "hsl(255, 11%, 22%)",
+        900: "hsl(260, 8%, 14%)",
+      },
+      danger: "hsl(0, 87%, 67%)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
